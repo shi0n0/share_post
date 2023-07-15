0a3a39302857a9d4b6e04eef9b0fe714411274fa
shi0n0 <shiono.yui1080@gmail.com>
2023-07-15 23:33:33 +0900

    ヘルパーを更新

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_messages
+    messages = flash.map do |_level, message|
+      content_tag(:div, class: 'alert') do
+        content_tag(:span, message, class: 'message')
+      end
+    end
+
+    safe_join(messages)
+  end
 end
